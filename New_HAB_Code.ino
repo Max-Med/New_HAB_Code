cb34e083d11cd5f23f22f4b2473ac005d8a97f44
Max-Med <maxmed17@gmail.com>
2014-07-25 21:10:17 +0100

    code Mk2
    Moved declaration of variables to within correct function,
    changed function declarations for time parameters, moved if/else
    statements to save power when gps doesn't have a fix

--- New_HAB_Code.ino
+++ New_HAB_Code.ino
@@ -1,21 +1,28 @@
 #include <TinyGPS.h>
 TinyGPS gps;
-float flat, flon;
-unsigned long fix_age, time, date, count, alt;
-int year;
-byte month, day, hour, minute, second, hundredths;
-char lat[10], lon[10], datastring[100];
-
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  float flat, flon;
+  unsigned long fix_age, time, date, count, alt;
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  char lat[10], lon[10], datastring[100];
+  
   while  (Serial.available()) {
     int c = Serial.read(); 
     if (gps.encode(c))  {
       gps.get_datetime(&date, &time, &fix_age);
+      
+      if (fix_age == TinyGPS::GPS_INVALID_AGE){
+        snprintf(datastring,sizeof(datastring),"$$MAX,no fix detected");
+      }
+      
+      else {
+      
       gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &fix_age);
       
       gps.f_get_position(&flat, &flon , &fix_age);
@@ -25,13 +32,10 @@ void loop() {
       
       alt= gps.altitude()/100;
       
-      if (fix_age == TinyGPS::GPS_INVALID_AGE){
-        snprintf(datastring,sizeof(datastring),"$$MAX,no fix detected");
+      snprintf(datastring,sizeof(datastring),"$$MAX,%lu,%02u:%02u:%02u,%s,%s,%lu",count,hour,minute,second,lat,lon,alt);
+      count = count + 1;
       }
-      else {
-        snprintf(datastring,sizeof(datastring),"$$MAX,%u,%u:%u:&u,%s,%s,%u",count,hour,minute,second,lat,lon,alt);
-        count = count + 1;
-      }
+      
       //radio + checksum stuff goes in here
     }
   }
